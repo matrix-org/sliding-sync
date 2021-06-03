8d640a7c1666d1e16fd6d9a379ab0a201e9de367
Kegan Dougal <kegan@matrix.org>
2021-06-03 15:23:07 +0100

    GH action support

--- state/main_test.go
+++ state/main_test.go
@@ -10,31 +10,55 @@ import (
 
 var postgresConnectionString = "user=xxxxx dbname=syncv3_test sslmode=disable"
 
-func TestMain(m *testing.M) {
+func createLocalDB() string {
 	fmt.Println("Note: tests require a postgres install accessible to the current user")
 	dbName := "syncv3_test"
-	// cleanup if we died mid-way last time, hence don't check err output in case it was already deleted
 	exec.Command("dropdb", dbName).Run()
+	if err := exec.Command("createdb", dbName).Run(); err != nil {
+		fmt.Println("createdb failed: ", err)
+		os.Exit(2)
+	}
+	return dbName
+}
 
+func currentUser() string {
 	user, err := user.Current()
 	if err != nil {
 		fmt.Println("cannot get current user: ", err)
 		os.Exit(2)
 	}
-	postgresConnectionString = fmt.Sprintf(
-		"user=%s dbname=%s sslmode=disable",
-		user.Username, dbName,
-	)
+	return user.Username
+}
 
-	if err := exec.Command("createdb", dbName).Run(); err != nil {
-		fmt.Println("createdb failed: ", err)
-		os.Exit(2)
+func prepareDBConnectionString() (connStr string) {
+	// Required vars: user and db
+	// We'll try to infer from the local env if they are missing
+	user := os.Getenv("POSTGRES_USER")
+	if user == "" {
+		user = currentUser()
 	}
+	dbName := os.Getenv("POSTGRES_DB")
+	if dbName == "" {
+		dbName = createLocalDB()
+	}
+	connStr = fmt.Sprintf(
+		"user=%s dbname=%s sslmode=disable",
+		user, dbName,
+	)
+	// optional vars, used in CI
+	password := os.Getenv("POSTGRES_PASSWORD")
+	if password != "" {
+		connStr += fmt.Sprintf(" password=%s", password)
+	}
+	host := os.Getenv("POSTGRES_HOST")
+	if host != "" {
+		connStr += fmt.Sprintf(" host=%s", host)
+	}
+	return
+}
 
+func TestMain(m *testing.M) {
+	postgresConnectionString = prepareDBConnectionString()
 	exitCode := m.Run()
-
-	// cleanup
-	fmt.Println("cleaning up database")
-	exec.Command("dropdb", dbName).Run()
 	os.Exit(exitCode)
 }
