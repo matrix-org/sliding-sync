c34890424f420f15294d5c81ffb303027b8715c2
Kegan Dougal <kegan@matrix.org>
2021-10-07 10:52:42 +0100

    factor out some connstate tests

--- sync3/connstate_test.go
+++ sync3/connstate_test.go
@@ -9,14 +9,26 @@ import (
 	"testing"
 )
 
+func newSortableRoom(roomID string, lastMsgTimestamp int64) SortableRoom {
+	return SortableRoom{
+		RoomID:               roomID,
+		Name:                 "Room " + roomID,
+		LastMessageTimestamp: lastMsgTimestamp,
+		LastEventJSON: json.RawMessage(
+			fmt.Sprintf(`{"type":"m.room.message","content":{"body":"hello"},"origin_server_ts":%d}`, lastMsgTimestamp),
+		),
+	}
+}
+
 type connStateStoreMock struct {
-	roomIDToRoom        map[string]*SortableRoom
+	roomIDToRoom        map[string]SortableRoom
 	userIDToJoinedRooms map[string][]string
 	userIDToPosition    map[string]int64
 }
 
 func (s *connStateStoreMock) LoadRoom(roomID string) *SortableRoom {
-	return s.roomIDToRoom[roomID]
+	sr := s.roomIDToRoom[roomID]
+	return &sr
 }
 func (s *connStateStoreMock) Load(userID string) (joinedRoomIDs []string, initialLoadPosition int64, err error) {
 	joinedRoomIDs = s.userIDToJoinedRooms[userID]
@@ -45,31 +57,20 @@ func TestConnStateInitial(t *testing.T) {
 		DeviceID:  "d",
 	}
 	userID := "@alice:localhost"
-	roomA := "!a:localhost"
-	roomB := "!b:localhost"
-	roomC := "!c:localhost"
 	timestampNow := int64(1632131678061)
+	roomA := newSortableRoom("!a:localhost", timestampNow-8000)
+	roomB := newSortableRoom("!b:localhost", timestampNow)
+	roomC := newSortableRoom("!c:localhost", timestampNow-4000)
+
 	// initial sort order B, C, A
 	csm := &connStateStoreMock{
 		userIDToJoinedRooms: map[string][]string{
-			userID: {roomA, roomB, roomC},
+			userID: {roomA.RoomID, roomB.RoomID, roomC.RoomID},
 		},
-		roomIDToRoom: map[string]*SortableRoom{
-			roomA: {
-				RoomID:               roomA,
-				Name:                 "Room A",
-				LastMessageTimestamp: timestampNow - 8000,
-			},
-			roomB: {
-				RoomID:               roomB,
-				Name:                 "Room B",
-				LastMessageTimestamp: timestampNow,
-			},
-			roomC: {
-				RoomID:               roomC,
-				Name:                 "Room C",
-				LastMessageTimestamp: timestampNow - 4000,
-			},
+		roomIDToRoom: map[string]SortableRoom{
+			roomA.RoomID: roomA,
+			roomB.RoomID: roomB,
+			roomC.RoomID: roomC,
 		},
 	}
 	cs := NewConnState(userID, csm)
@@ -93,16 +94,19 @@ func TestConnStateInitial(t *testing.T) {
 				Range:     []int64{0, 9},
 				Rooms: []Room{
 					{
-						RoomID: roomB,
-						Name:   "Room B",
+						RoomID:   roomB.RoomID,
+						Name:     roomB.Name,
+						Timeline: []json.RawMessage{roomB.LastEventJSON},
 					},
 					{
-						RoomID: roomC,
-						Name:   "Room C",
+						RoomID:   roomC.RoomID,
+						Name:     roomC.Name,
+						Timeline: []json.RawMessage{roomC.LastEventJSON},
 					},
 					{
-						RoomID: roomA,
-						Name:   "Room A",
+						RoomID:   roomA.RoomID,
+						Name:     roomA.Name,
+						Timeline: []json.RawMessage{roomA.LastEventJSON},
 					},
 				},
 			},
@@ -112,7 +116,7 @@ func TestConnStateInitial(t *testing.T) {
 	// bump A to the top
 	csm.PushNewEvent(cs, &EventData{
 		event:     json.RawMessage(`{}`),
-		roomID:    roomA,
+		roomID:    roomA.RoomID,
 		eventType: "unimportant",
 		timestamp: timestampNow + 1000,
 	})
@@ -138,7 +142,7 @@ func TestConnStateInitial(t *testing.T) {
 				Operation: "INSERT",
 				Index:     intPtr(0),
 				Room: &Room{
-					RoomID: roomA,
+					RoomID: roomA.RoomID,
 				},
 			},
 		},
@@ -147,7 +151,7 @@ func TestConnStateInitial(t *testing.T) {
 	// another message should just update
 	csm.PushNewEvent(cs, &EventData{
 		event:     json.RawMessage(`{}`),
-		roomID:    roomA,
+		roomID:    roomA.RoomID,
 		eventType: "still unimportant",
 		timestamp: timestampNow + 2000,
 	})
@@ -167,13 +171,14 @@ func TestConnStateInitial(t *testing.T) {
 				Operation: "UPDATE",
 				Index:     intPtr(0),
 				Room: &Room{
-					RoomID: roomA,
+					RoomID: roomA.RoomID,
 				},
 			},
 		},
 	})
 }
 
+// Test that multiple ranges can be tracked in a single request
 func TestConnStateMultipleRanges(t *testing.T) {
 	connID := ConnID{
 		SessionID: "s",
@@ -181,12 +186,12 @@ func TestConnStateMultipleRanges(t *testing.T) {
 	}
 	userID := "@alice:localhost"
 	timestampNow := int64(1632131678061)
-	var rooms []*SortableRoom
+	var rooms []SortableRoom
 	var roomIDs []string
-	roomIDToRoom := make(map[string]*SortableRoom)
+	roomIDToRoom := make(map[string]SortableRoom)
 	for i := 0; i < 10; i++ {
 		roomID := fmt.Sprintf("!%d:localhost", i)
-		room := &SortableRoom{
+		room := SortableRoom{
 			RoomID: roomID,
 			Name:   fmt.Sprintf("Room %d", i),
 			// room 1 is most recent, 10 is least recent
@@ -347,48 +352,30 @@ func TestConnStateMultipleRanges(t *testing.T) {
 	})
 }
 
+// Test that room subscriptions can be made and that events are pushed for them.
 func TestConnStateRoomSubscriptions(t *testing.T) {
 	connID := ConnID{
 		SessionID: "s",
 		DeviceID:  "d",
 	}
 	userID := "@alice:localhost"
-	roomA := "!a:localhost"
-	roomB := "!b:localhost"
-	roomC := "!c:localhost"
-	roomD := "!d:localhost"
-	roomIDs := []string{roomA, roomB, roomC, roomD}
 	timestampNow := int64(1632131678061)
+	roomA := newSortableRoom("!a:localhost", timestampNow)
+	roomB := newSortableRoom("!b:localhost", timestampNow)
+	roomC := newSortableRoom("!c:localhost", timestampNow)
+	roomD := newSortableRoom("!d:localhost", timestampNow)
+	roomIDs := []string{roomA.RoomID, roomB.RoomID, roomC.RoomID, roomD.RoomID}
+
 	// initial sort order A,B,C,D
 	csm := &connStateStoreMock{
 		userIDToJoinedRooms: map[string][]string{
 			userID: roomIDs,
 		},
-		roomIDToRoom: map[string]*SortableRoom{
-			roomA: {
-				RoomID:               roomA,
-				Name:                 "Room A",
-				LastMessageTimestamp: timestampNow,
-				LastEventJSON:        json.RawMessage(fmt.Sprintf(`{"origin_server_ts":%d, "type":"m.room.message", "a":"yep"}`, timestampNow)),
-			},
-			roomB: {
-				RoomID:               roomB,
-				Name:                 "Room B",
-				LastMessageTimestamp: timestampNow - 2000,
-				LastEventJSON:        json.RawMessage(fmt.Sprintf(`{"origin_server_ts":%d, "type":"m.room.message", "b":"yep"}`, timestampNow-2000)),
-			},
-			roomC: {
-				RoomID:               roomC,
-				Name:                 "Room C",
-				LastMessageTimestamp: timestampNow - 4000,
-				LastEventJSON:        json.RawMessage(fmt.Sprintf(`{"origin_server_ts":%d, "type":"m.room.message", "c":"yep"}`, timestampNow-4000)),
-			},
-			roomD: {
-				RoomID:               roomD,
-				Name:                 "Room D",
-				LastMessageTimestamp: timestampNow - 6000,
-				LastEventJSON:        json.RawMessage(fmt.Sprintf(`{"origin_server_ts":%d, "type":"m.room.message", "d":"yep"}`, timestampNow-6000)),
-			},
+		roomIDToRoom: map[string]SortableRoom{
+			roomA.RoomID: roomA,
+			roomB.RoomID: roomB,
+			roomC.RoomID: roomC,
+			roomD.RoomID: roomD,
 		},
 	}
 	cs := NewConnState(userID, csm)
@@ -396,7 +383,7 @@ func TestConnStateRoomSubscriptions(t *testing.T) {
 	res, err := cs.HandleIncomingRequest(context.Background(), connID, &Request{
 		Sort: []string{SortByRecency},
 		RoomSubscriptions: map[string]RoomSubscription{
-			roomD: {
+			roomD.RoomID: {
 				TimelineLimit: 20,
 			},
 		},
@@ -410,11 +397,11 @@ func TestConnStateRoomSubscriptions(t *testing.T) {
 	checkResponse(t, false, res, &Response{
 		Count: int64(len(roomIDs)),
 		RoomSubscriptions: map[string]Room{
-			roomD: {
-				RoomID: roomD,
-				Name:   "Room D",
+			roomD.RoomID: {
+				RoomID: roomD.RoomID,
+				Name:   roomD.Name,
 				Timeline: []json.RawMessage{
-					csm.roomIDToRoom[roomD].LastEventJSON,
+					roomD.LastEventJSON,
 				},
 			},
 		},
@@ -444,7 +431,7 @@ func TestConnStateRoomSubscriptions(t *testing.T) {
 	// room D gets a new event
 	csm.PushNewEvent(cs, &EventData{
 		event:     json.RawMessage(`{}`),
-		roomID:    roomD,
+		roomID:    roomD.RoomID,
 		eventType: "unimportant",
 		timestamp: timestampNow + 2000,
 	})
@@ -461,8 +448,8 @@ func TestConnStateRoomSubscriptions(t *testing.T) {
 	checkResponse(t, false, res, &Response{
 		Count: int64(len(roomIDs)),
 		RoomSubscriptions: map[string]Room{
-			roomD: {
-				RoomID: roomD,
+			roomD.RoomID: {
+				RoomID: roomD.RoomID,
 				Timeline: []json.RawMessage{
 					json.RawMessage(`{}`),
 				},
@@ -475,11 +462,11 @@ func TestConnStateRoomSubscriptions(t *testing.T) {
 	res, err = cs.HandleIncomingRequest(context.Background(), connID, &Request{
 		Sort: []string{SortByRecency},
 		RoomSubscriptions: map[string]RoomSubscription{
-			roomC: {
+			roomC.RoomID: {
 				TimelineLimit: 20,
 			},
 		},
-		UnsubscribeRooms: []string{roomD},
+		UnsubscribeRooms: []string{roomD.RoomID},
 		Rooms: SliceRanges([][2]int64{
 			{0, 1},
 		}),
@@ -490,11 +477,11 @@ func TestConnStateRoomSubscriptions(t *testing.T) {
 	checkResponse(t, false, res, &Response{
 		Count: int64(len(roomIDs)),
 		RoomSubscriptions: map[string]Room{
-			roomC: {
-				RoomID: roomC,
-				Name:   "Room C",
+			roomC.RoomID: {
+				RoomID: roomC.RoomID,
+				Name:   roomC.Name,
 				Timeline: []json.RawMessage{
-					csm.roomIDToRoom[roomC].LastEventJSON,
+					roomC.LastEventJSON,
 				},
 			},
 		},
