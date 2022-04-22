5a91264d2ff51a84f85b4f8b791edf502aab9522
Kegan Dougal <kegan@matrix.org>
2022-04-22 12:35:44 +0100

    v0.1.2

--- cmd/syncv3/main.go
+++ cmd/syncv3/main.go
@@ -15,7 +15,7 @@ import (
 
 var GitCommit string
 
-const version = "0.1.1"
+const version = "0.1.2"
 
 var (
 	flagDestinationServer = flag.String("server", "", "The destination v2 matrix server")
