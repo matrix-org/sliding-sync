ba78a33bb883690f89d463ccc03e0a0df15be481
Kegan Dougal <kegan@matrix.org>
2022-08-16 11:13:03 +0100

    bugfix: use a map to store joined room tracker info
    Previously we used a slice as this is slightly cheaper, but
    since Synapse can return multiple join events in the timeline
    it could cause a user ID to be present multiple times in a room.
    
    When this happened, it would cause the `UserCache` callbacks to
    be invoked twice for every event, clearly not ideal. By using a
    set instead, we make sure that we don't add the same user more
    than one time.
    
    Ref: https://github.com/matrix-org/synapse/issues/9768

--- sync3/tracker.go
+++ sync3/tracker.go
@@ -4,6 +4,8 @@ import (
 	"sync"
 )
 
+type set map[string]struct{}
+
 // Tracks who is joined to which rooms. This is critical from a security perspective in order to
 // ensure that only the users joined to the room receive events in that room. Consider the situation
 // where Alice and Bob are joined to room X. If Alice gets kicked from X, the proxy server will still
@@ -11,15 +13,15 @@ import (
 // active connections should be pushed events, which is what this tracker does.
 type JoinedRoomsTracker struct {
 	// map of room_id to joined user IDs.
-	roomIDToJoinedUsers map[string][]string
-	userIDToJoinedRooms map[string][]string
+	roomIDToJoinedUsers map[string]set
+	userIDToJoinedRooms map[string]set
 	mu                  *sync.RWMutex
 }
 
 func NewJoinedRoomsTracker() *JoinedRoomsTracker {
 	return &JoinedRoomsTracker{
-		roomIDToJoinedUsers: make(map[string][]string),
-		userIDToJoinedRooms: make(map[string][]string),
+		roomIDToJoinedUsers: make(map[string]set),
+		userIDToJoinedRooms: make(map[string]set),
 		mu:                  &sync.RWMutex{},
 	}
 }
@@ -28,7 +30,7 @@ func (t *JoinedRoomsTracker) IsUserJoined(userID, roomID string) bool {
 	t.mu.RLock()
 	defer t.mu.RUnlock()
 	users := t.roomIDToJoinedUsers[roomID]
-	for _, u := range users {
+	for u := range users {
 		if u == userID {
 			return true
 		}
@@ -42,15 +44,21 @@ func (t *JoinedRoomsTracker) UserJoinedRoom(userID, roomID string) bool {
 	defer t.mu.Unlock()
 	wasJoined := false
 	users := t.roomIDToJoinedUsers[roomID]
-	for _, u := range users {
+	for u := range users {
 		if u == userID {
 			wasJoined = true
 		}
 	}
 	joinedRooms := t.userIDToJoinedRooms[userID]
+	if joinedRooms == nil {
+		joinedRooms = make(set)
+	}
 	joinedUsers := t.roomIDToJoinedUsers[roomID]
-	joinedRooms = append(joinedRooms, roomID)
-	joinedUsers = append(joinedUsers, userID)
+	if joinedUsers == nil {
+		joinedUsers = make(set)
+	}
+	joinedRooms[roomID] = struct{}{}
+	joinedUsers[userID] = struct{}{}
 	t.userIDToJoinedRooms[userID] = joinedRooms
 	t.roomIDToJoinedUsers[roomID] = joinedUsers
 	return !wasJoined
@@ -60,21 +68,9 @@ func (t *JoinedRoomsTracker) UserLeftRoom(userID, roomID string) {
 	t.mu.Lock()
 	defer t.mu.Unlock()
 	joinedRooms := t.userIDToJoinedRooms[userID]
-	for i := 0; i < len(joinedRooms); i++ {
-		if roomID == joinedRooms[i] {
-			// remove without preserving order
-			joinedRooms[i] = joinedRooms[len(joinedRooms)-1]
-			joinedRooms = joinedRooms[:len(joinedRooms)-1]
-		}
-	}
+	delete(joinedRooms, roomID)
 	joinedUsers := t.roomIDToJoinedUsers[roomID]
-	for i := 0; i < len(joinedUsers); i++ {
-		if userID == joinedUsers[i] {
-			// remove without preserving order
-			joinedUsers[i] = joinedUsers[len(joinedUsers)-1]
-			joinedUsers = joinedUsers[:len(joinedUsers)-1]
-		}
-	}
+	delete(joinedUsers, userID)
 	t.userIDToJoinedRooms[userID] = joinedRooms
 	t.roomIDToJoinedUsers[roomID] = joinedUsers
 }
@@ -82,21 +78,31 @@ func (t *JoinedRoomsTracker) JoinedRoomsForUser(userID string) []string {
 	t.mu.RLock()
 	defer t.mu.RUnlock()
 	rooms := t.userIDToJoinedRooms[userID]
-	if len(rooms) == 0 {
+	if rooms == nil || len(rooms) == 0 {
 		return nil
 	}
-	roomsCopy := make([]string, len(rooms))
-	copy(roomsCopy, rooms)
-	return roomsCopy
+	n := len(rooms)
+	i := 0
+	result := make([]string, n)
+	for roomID := range rooms {
+		result[i] = roomID
+		i++
+	}
+	return result
 }
 func (t *JoinedRoomsTracker) JoinedUsersForRoom(roomID string) []string {
 	t.mu.RLock()
 	defer t.mu.RUnlock()
 	users := t.roomIDToJoinedUsers[roomID]
-	if len(users) == 0 {
+	if users == nil || len(users) == 0 {
 		return nil
 	}
-	usersCopy := make([]string, len(users))
-	copy(usersCopy, users)
-	return usersCopy
+	n := len(users)
+	i := 0
+	result := make([]string, n)
+	for userID := range users {
+		result[i] = userID
+		i++
+	}
+	return result
 }
