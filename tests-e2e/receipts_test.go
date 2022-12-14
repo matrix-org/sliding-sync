42e34b9eadefba28b24265a16ee423f364df09f0
Kegan Dougal <kegan@matrix.org>
2022-12-14 19:04:10 +0000

    Race condition on private receipts

--- tests-e2e/receipts_test.go
+++ tests-e2e/receipts_test.go
@@ -2,6 +2,7 @@ package syncv3_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/matrix-org/sync-v3/sync3"
 	"github.com/matrix-org/sync-v3/sync3/extensions"
@@ -183,6 +184,7 @@ func TestReceiptsPrivate(t *testing.T) {
 	})
 	// bob secretly reads this
 	bob.SendReceipt(t, roomID, eventID, "m.read.private")
+	time.Sleep(100 * time.Millisecond) // TODO: find a better way to wait until the proxy has processed this.
 	// alice does sliding sync -> does not see private RR
 	res := alice.SlidingSync(t, sync3.Request{
 		RoomSubscriptions: map[string]sync3.RoomSubscription{
