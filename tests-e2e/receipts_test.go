3070292fba597ec9a921de9e015f94dba3afdccc
Kegan Dougal <kegan@matrix.org>
2022-12-15 10:11:46 +0000

    Fix failing receipts test; use a newer synapse version

--- tests-e2e/receipts_test.go
+++ tests-e2e/receipts_test.go
@@ -184,7 +184,7 @@ func TestReceiptsPrivate(t *testing.T) {
 	})
 	// bob secretly reads this
 	bob.SendReceipt(t, roomID, eventID, "m.read.private")
-	time.Sleep(time.Second) // TODO: find a better way to wait until the proxy has processed this.
+	time.Sleep(300 * time.Millisecond) // TODO: find a better way to wait until the proxy has processed this.
 	// alice does sliding sync -> does not see private RR
 	res := alice.SlidingSync(t, sync3.Request{
 		RoomSubscriptions: map[string]sync3.RoomSubscription{
