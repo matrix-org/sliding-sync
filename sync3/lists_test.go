a2888b6d4183a3d1519947479eb868009f7fdaa7
Kegan Dougal <kegan@matrix.org>
2023-02-02 15:00:34 +0000

    Add Sort benchmark

---
+++ sync3/lists_test.go
@@ -0,0 +1,44 @@
+package sync3_test
+
+import (
+	"fmt"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/matrix-org/sliding-sync/internal"
+	"github.com/matrix-org/sliding-sync/sync3"
+	"github.com/matrix-org/sliding-sync/sync3/caches"
+)
+
+var roomCounter atomic.Int64
+var timestamp = time.Now()
+
+func BenchmarkSortRooms(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		sortRooms(4000)
+	}
+}
+
+func sortRooms(n int) {
+	list := sync3.NewInternalRequestLists()
+	addRooms(list, n)
+	list.AssignList("benchmark", &sync3.RequestFilters{}, []string{sync3.SortByRecency}, sync3.Overwrite)
+}
+
+func addRooms(list *sync3.InternalRequestLists, n int) {
+	for i := 0; i < n; i++ {
+		next := roomCounter.Add(1)
+		list.SetRoom(sync3.RoomConnMetadata{
+			RoomMetadata: internal.RoomMetadata{
+				RoomID:               fmt.Sprintf("!%d:benchmark", next),
+				JoinCount:            int(next % 100),
+				NameEvent:            fmt.Sprintf("Room %d", next),
+				LastMessageTimestamp: uint64(timestamp.Add(time.Duration(next) * time.Minute).UnixMilli()),
+			},
+			UserRoomData: caches.UserRoomData{
+				IsDM: next%10 == 0,
+			},
+		})
+	}
+}
