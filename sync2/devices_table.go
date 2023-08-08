188e10e77bdb1d43cc984b5becf257ac20407943
David Robertson <davidr@element.io>
2023-08-08 18:48:16 +0100

    Add function to query for old devices

--- sync2/devices_table.go
+++ sync2/devices_table.go
@@ -3,6 +3,7 @@ package sync2
 import (
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
+	"time"
 )
 
 type Device struct {
@@ -44,4 +45,23 @@ func (t *DevicesTable) InsertDevice(txn *sqlx.Tx, userID, deviceID string) error
 func (t *DevicesTable) UpdateDeviceSince(userID, deviceID, since string) error {
 	_, err := t.db.Exec(`UPDATE syncv3_sync2_devices SET since = $1 WHERE user_id = $2 AND device_id = $3`, since, userID, deviceID)
 	return err
+}
+
+// FindOldDevices fetches the user_id and device_id of all devices which haven't /synced
+// for at least as long as the given inactivityPeriod. Such devices are returned in
+// no particular order.
+//
+// This is determined using the syncv3_sync2_tokens.last_seen column, which is updated
+// at most once per day to save DB throughtput (see MaybeUpdateLastSeen). The caller
+// should therefore use an inactivityPeriod of at least two days to avoid considering
+// a recently-used device as old.
+func (t *DevicesTable) FindOldDevices(inactivityPeriod time.Duration) (devices []Device, err error) {
+	err = t.db.Select(&devices, `
+		SELECT user_id, device_id
+		FROM syncv3_sync2_devices JOIN syncv3_sync2_tokens USING(user_id, device_id)
+		GROUP BY (user_id, device_id)
+		HAVING MAX(last_seen) < $1
+	`, time.Now().Add(-inactivityPeriod),
+	)
+	return
 }
