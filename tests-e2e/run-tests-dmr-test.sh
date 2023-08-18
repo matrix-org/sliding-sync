4825f2d08b5375a9fa92e8325da77e1fb32373c6
David Robertson <davidr@element.io>
2023-08-18 16:03:33 +0100

    WIPWIP

---
+++ tests-e2e/run-tests-dmr-test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -eu
+export SYNCV3_BINDADDR=0.0.0.0:8844
+export SYNCV3_ADDR='http://localhost:8844'
+export SYNCV3_DEBUG=1
+
+# Run synapse and stop it afterwards.
+python -m synapse.app.homeserver -c synapse/homeserver.yaml &
+SYNAPSE_PID=$!
+
+# Run the binary and stop it afterwards.
+# Direct stderr into stdout, and optionally redirect both to a file.
+../syncv3 &> "${E2E_TEST_SERVER_STDOUT:-/dev/stdout}" &
+SYNCV3_PID=$!
+trap "kill $SYNCV3_PID; kill $SYNAPSE_PID" EXIT
+
+# wait for the server to be listening, we want this endpoint to 404 instead of connrefused
+until [ \
+  "$(curl -s -w '%{http_code}' -o /dev/null "http://localhost:8844/idonotexist")" \
+  -eq 404 ]
+do
+  echo 'Waiting for server to start...'
+  sleep 1
+done
+
+go test "$@"
