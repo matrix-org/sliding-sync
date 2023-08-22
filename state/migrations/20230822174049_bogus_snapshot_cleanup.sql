68b5eb06bd72fc1cad165f395d8272117258f19a
David Robertson <davidr@element.io>
2023-08-22 18:02:40 +0100

    Goose runs its migration in a txn by default
    According to https://github.com/pressly/goose#sql-migrations, anyway

--- state/migrations/20230822174049_bogus_snapshot_cleanup.sql
+++ state/migrations/20230822174049_bogus_snapshot_cleanup.sql
@@ -1,8 +1,6 @@
 -- +goose Up
 -- This migration runs a one-time cleanup job to delete rooms with dodgy state snapshots.
 -- A room is dodgy if its current snapshot lacks a create event.
-BEGIN;
-
 CREATE TEMPORARY TABLE bogus_rooms AS (
     SELECT room_id, current_snapshot_id
     FROM syncv3_rooms
@@ -25,8 +23,5 @@ WHERE room_id IN (SELECT room_id FROM bogus_rooms);
 DELETE
 FROM syncv3_rooms
 WHERE room_id IN (SELECT room_id FROM bogus_rooms);
-
-COMMIT;
-
 -- +goose Down
 -- downgrading is a no-op.
