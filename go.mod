9ae4a04824f34c14419c1c1b0043df98b3727056
Kegan Dougal <kegan@matrix.org>
2023-08-23 12:52:47 +0100

    Move from Jaeger to OTLP exportor
    Jaeger spans can be sent as OTLP so this is mostly semantics for
    the collector, which is more flexible if it accepts OTLP traces
    rather than jaeger.thrift traces.

--- go.mod
+++ go.mod
@@ -25,12 +25,14 @@ require (
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/felixge/httpsnoop v1.0.3 // indirect
 	github.com/fxamacker/cbor/v2 v2.5.0 // indirect
 	github.com/go-logr/logr v1.2.4 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.17.0 // indirect
 	github.com/matrix-org/gomatrix v0.0.0-20210324163249-be2af5ef2e16 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
@@ -43,10 +45,18 @@ require (
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/internal/retry v1.16.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.16.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.16.0 // indirect
 	go.opentelemetry.io/otel/metric v1.16.0 // indirect
-	golang.org/x/crypto v0.10.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.0.0 // indirect
+	golang.org/x/crypto v0.12.0 // indirect
+	golang.org/x/net v0.14.0 // indirect
 	golang.org/x/sync v0.2.0 // indirect
-	golang.org/x/sys v0.10.0 // indirect
-	golang.org/x/text v0.11.0 // indirect
-	google.golang.org/protobuf v1.29.1 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230822172742-b8732ec3820d // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
+	google.golang.org/grpc v1.57.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
--- internal/trace.go
+++ internal/trace.go
@@ -2,18 +2,19 @@ package internal
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
 	"runtime/trace"
 
-	"github.com/go-logr/zerologr" // required for Jaeger errors during transmission to use zerolog
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	tracesdk "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
-	otlptrace "go.opentelemetry.io/otel/trace"
+	otrace "go.opentelemetry.io/otel/trace"
 )
 
 const tracerName = "sliding-sync"
@@ -26,7 +27,7 @@ var (
 
 type Task struct {
 	t *trace.Task
-	o otlptrace.Span
+	o otrace.Span
 }
 
 func (s *Task) End() {
@@ -37,7 +38,7 @@ func (s *Task) End() {
 // combined runtime/trace and OTLP span
 type RuntimeTraceOTLPSpan struct {
 	region *trace.Region
-	span   otlptrace.Span
+	span   otrace.Span
 }
 
 func (s *RuntimeTraceOTLPSpan) End() {
@@ -47,8 +48,8 @@ func (s *RuntimeTraceOTLPSpan) End() {
 
 func Logf(ctx context.Context, category, format string, args ...interface{}) {
 	trace.Logf(ctx, category, format, args...)
-	s := otlptrace.SpanFromContext(ctx)
-	s.AddEvent(fmt.Sprintf(format, args...), otlptrace.WithAttributes(
+	s := otrace.SpanFromContext(ctx)
+	s.AddEvent(fmt.Sprintf(format, args...), otrace.WithAttributes(
 		attribute.String("category", category),
 	))
 }
@@ -72,12 +73,21 @@ func StartTask(ctx context.Context, name string) (context.Context, *Task) {
 	}
 }
 
-func ConfigureJaeger(jaegerURL, version string) error {
-	_ = zerologr.New(&logger) // TODO
-	// Create the Jaeger exporter
-	exp, err := jaeger.New(jaeger.WithCollectorEndpoint(
-		jaeger.WithEndpoint(jaegerURL),
-	))
+func ConfigureOTLP(otlpURL, otlpUser, otlpPass, version string) error {
+	ctx := context.Background()
+	opts := []otlptracehttp.Option{
+		otlptracehttp.WithEndpoint(otlpURL),
+	}
+	if otlpPass != "" && otlpUser != "" {
+		opts = append(opts, otlptracehttp.WithHeaders(
+			map[string]string{
+				"Authorization": fmt.Sprintf("Basic %s", base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%s:%s", otlpUser, otlpPass)))),
+			},
+		))
+	}
+	client := otlptracehttp.NewClient(opts...)
+	// Create the OTLP exporter
+	exp, err := otlptrace.New(ctx, client)
 	if err != nil {
 		return err
 	}
