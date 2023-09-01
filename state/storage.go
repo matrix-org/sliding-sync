2b06bf3d7931f4847f22db39a61e66b9c7a37324
Till Faelligen <2353100+S7evinK@users.noreply.github.com>
2023-09-01 14:29:31 +0200

    Clean up

--- state/storage.go
+++ state/storage.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"os"
 	"strings"
-	"time"
 
 	"github.com/getsentry/sentry-go"
 	"github.com/lib/pq"
@@ -501,38 +500,37 @@ func (s *Storage) RoomStateAfterEventPosition(ctx context.Context, roomIDs []str
 			args = append(args, pq.StringArray(userIDs))
 			args = append(args, typeArgs...)
 
-			// figure out which state events to look at - if there is no m.room.member filter we can be super fast
-			needUnion := false
-			if hasMembershipFilter {
-				needUnion = true
+			// If there are no where clauses, we need to add a "dummy" where, so we don't get
+			// anything back. This is mostly the case if we only care about membership events,
+			// which aren't added in the above loop.
+			if len(wheres) == 0 {
+				wheres = append(wheres, "1>1")
 			}
-			// it's not possible for there to be no membership filter and no other filter, we wouldn't be executing this code
-			// it is possible to have both, so neither if will execute.
 
 			// Similar to CurrentStateEventsInAllRooms
 			// We're using a CTE here, since unnestting the nids is quite expensive. Using the array as is
 			// and using ANY() instead performs quite well (e.g. 86k membership events and 130ms execution time, vs
 			// the previous query with unnest took 2.5s)
-			if len(wheres) == 0 {
-				wheres = append(wheres, "1=1")
-			}
-			qry := `WITH nids AS (
-    SELECT snapshot_id, events, membership_events FROM syncv3_snapshots WHERE snapshot_id = ANY(?)
-), memberships AS (
-    SELECT syncv3_memberships.event_nid
-    FROM syncv3_memberships, nids
-    WHERE state_key = ANY (?) AND nids.snapshot_id = ANY(syncv3_memberships.snapshot_id)
-)
-SELECT syncv3_events.event_nid, syncv3_events.room_id, syncv3_events.event_type, syncv3_events.state_key, syncv3_events.event
-FROM syncv3_events, nids
-WHERE (syncv3_events.event_nid = ANY(nids.events) AND (` + strings.Join(wheres, " OR ") + `))`
+			qry := `
+				WITH nids AS (
+					SELECT snapshot_id, events, membership_events FROM syncv3_snapshots WHERE snapshot_id = ANY(?)
+				), memberships AS (
+    				SELECT syncv3_memberships.event_nid
+    				FROM syncv3_memberships, nids
+    				WHERE state_key = ANY (?) AND nids.snapshot_id = ANY(syncv3_memberships.snapshot_id)
+				)
+				SELECT syncv3_events.event_nid, syncv3_events.room_id, syncv3_events.event_type, syncv3_events.state_key, syncv3_events.event
+				FROM syncv3_events, nids
+				WHERE (syncv3_events.event_nid = ANY(nids.events) AND (` + strings.Join(wheres, " OR ") + `))`
 
-			if needUnion {
+			// If we're getting membership events, add the UNION select as well.
+			// This causes the CTE "memberships" to be executed.
+			if hasMembershipFilter {
 				qry += ` UNION
-SELECT syncv3_events.event_nid, syncv3_events.room_id, syncv3_events.event_type, syncv3_events.state_key, syncv3_events.event
-FROM syncv3_events, memberships
-WHERE syncv3_events.event_nid IN (memberships.event_nid)
-ORDER BY event_nid ASC`
+				SELECT syncv3_events.event_nid, syncv3_events.room_id, syncv3_events.event_type, syncv3_events.state_key, syncv3_events.event
+				FROM syncv3_events, memberships
+				WHERE syncv3_events.event_nid IN (memberships.event_nid)
+				ORDER BY event_nid ASC`
 			} else {
 				qry += ` ORDER BY event_nid ASC`
 			}
@@ -542,7 +540,6 @@ ORDER BY event_nid ASC`
 			if err != nil {
 				return fmt.Errorf("failed to form sql query: %s", err)
 			}
-			qryStart := time.Now()
 			rows, err := txn.Query(txn.Rebind(query), args...)
 			if err != nil {
 				return fmt.Errorf("failed to execute query: %s", err)
@@ -562,9 +559,7 @@ ORDER BY event_nid ASC`
 				roomToEvents[ev.RoomID] = append(roomToEvents[ev.RoomID], ev)
 				eventCount++
 			}
-			logger.Trace().Int("events", eventCount).Strs("rooms", roomIDs).Msgf("Query: %s", query)
-			logger.Trace().Int("events", eventCount).Strs("rooms", roomIDs).Msgf("Args: %#v", args)
-			logger.Trace().Int("events", eventCount).Strs("rooms", roomIDs).Msgf("%s - Received events from database", time.Since(qryStart))
+
 			// handle the most recent events which won't be in the snapshot but may need to be.
 			// we handle the replace case but don't handle brand new state events
 			for i := range latestEvents {
