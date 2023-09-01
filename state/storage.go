2131cb80b7ebe1739875a929550ecec322da407b
Till Faelligen <2353100+S7evinK@users.noreply.github.com>
2023-09-01 09:44:45 +0200

    Optimize for storage?

--- state/storage.go
+++ state/storage.go
@@ -518,7 +518,7 @@ func (s *Storage) RoomStateAfterEventPosition(ctx context.Context, roomIDs []str
 ), memberships AS (
     SELECT syncv3_memberships.event_nid
     FROM syncv3_memberships, nids
-    WHERE syncv3_memberships.snapshot_id IN (nids.snapshot_id) AND state_key = ANY (?)
+    WHERE WHERE state_key = ANY (?) AND nids.snapshot_id = ANY(syncv3_memberships.snapshot_id)
 )
 SELECT syncv3_events.event_nid, syncv3_events.room_id, syncv3_events.event_type, syncv3_events.state_key, syncv3_events.event
 FROM syncv3_events, nids
