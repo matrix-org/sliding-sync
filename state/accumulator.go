806639a92c72330299a9d844b31256974b9c9b21
Kegan Dougal <kegan@matrix.org>
2023-09-14 09:32:20 +0100

    Review comments

--- state/accumulator.go
+++ state/accumulator.go
@@ -238,6 +238,7 @@ func (a *Accumulator) Initialise(roomID string, state []json.RawMessage) (Initia
 				Str("room_id", roomID).
 				Int("len_state", len(events)).
 				Msg(errMsg)
+			// the HS gave us bad data so there's no point retrying => return DataError
 			return internal.NewDataError(errMsg)
 		}
 
@@ -389,6 +390,7 @@ func (a *Accumulator) Accumulate(txn *sqlx.Tx, userID, roomID string, prevBatch 
 				})
 				sentry.CaptureMessage(msg)
 			})
+			// the HS gave us bad data so there's no point retrying
 			// by not returning an error, we are telling the poller it is fine to not retry this request.
 			return 0, nil, nil
 		}
