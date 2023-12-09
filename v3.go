5777edbcad82016d4c45c9c8bd720c9c05f3a482
nisbet-hubbard <87453615+nisbet-hubbard@users.noreply.github.com>
2023-12-09 11:31:08 +0800

    Fix unix socket permissions

--- v3.go
+++ v3.go
@@ -249,7 +249,7 @@ func unixSocketListener(bindAddr string) net.Listener {
 		logger.Fatal().Err(err).Msg("failed to serve unix socket")
 	}
 	// TODO: safe default for now (rwxr-xr-x), could be extracted as env variable if needed
-	err = os.Chmod(bindAddr, 0755)
+	err = os.Chmod(bindAddr, 0220)
 	if err != nil {
 		logger.Fatal().Err(err).Msg("failed to set unix socket permissions")
 	}
